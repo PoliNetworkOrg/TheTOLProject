9003f9cb4f9f8dbd3f80428f9657b0b08762a7f0
Federico Grandi <fgrandi30@gmail.com>
2021-10-22 12:12:14 +0200

    fix(DBPreview): update databse preview

--- src/components/DBPreview.tsx
+++ src/components/DBPreview.tsx
@@ -19,26 +19,28 @@ export default function DBPreview({ db }: DBPreviewProps) {
         <div key={key}>
           <h3>Category: {key}</h3>
           <ul>
-            {questions.map((q) => (
-              <div key={key + q.id}>
-                <RenderedText text={q.text} />
-                <br />
-                <p>Valid: {q.validated}</p>
-                <br />
-                <RenderedText text={q.answers.a} />
-                <br />
-                <RenderedText text={q.answers.b} />
-                <br />
-                <RenderedText text={q.answers.c} />
-                <br />
-                <RenderedText text={q.answers.d} />
-                <br />
-                <RenderedText text={q.answers.e} />
-                <br />
-                <br />
-                <br />
-              </div>
-            ))}
+            {questions
+              .filter((q) => q.text || key == 'com')
+              .map((q) => (
+                <div key={key + q.id}>
+                  <RenderedText text={q.text} />
+                  <br />
+                  <p>Valid: {q.validated + ''}</p>
+                  <br />
+                  <RenderedText text={q.answers.a} />
+                  <br />
+                  <RenderedText text={q.answers.b} />
+                  <br />
+                  <RenderedText text={q.answers.c} />
+                  <br />
+                  <RenderedText text={q.answers.d} />
+                  <br />
+                  <RenderedText text={q.answers.e} />
+                  <br />
+                  <br />
+                  <br />
+                </div>
+              ))}
           </ul>
         </div>
       ))}
