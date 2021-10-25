c947dcb4c367ed8fef7d11dec4c09a5aaff721b6
Federico Grandi <fgrandi30@gmail.com>
2021-10-25 23:11:51 +0200

    fix: add placeholders for no questions and attachments

--- src/components/QuestionsForm/QuestionView.tsx
+++ src/components/QuestionsForm/QuestionView.tsx
@@ -10,9 +10,19 @@ interface QuestionViewProps {
 const textStyle = createStyle()
 
 export default function QuestionView({ question }: QuestionViewProps) {
+  if (!question)
+    return <span style={textStyle}>No question to display ¯\_(ツ)_/¯</span>
+
   return (
     <div style={textStyle}>
       <RenderedText text={question.text}></RenderedText>
+      <br />
+      <br />
+      <span>
+        {question.attachments?.length
+          ? 'TODO ' + question.attachments.join(', ')
+          : ''}
+      </span>
     </div>
   )
 }
