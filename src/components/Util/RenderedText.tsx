fa06f06dcfa064b62ac97a17e6c9f8ce7efdd22d
Federico Grandi <fgrandi30@gmail.com>
2021-10-28 19:40:22 +0200

    chore: install typings for react-latex

--- src/components/Util/RenderedText.tsx
+++ src/components/Util/RenderedText.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-// @ts-expect-error Waiting for DefinitelyTyped PR to be merged: https://github.com/DefinitelyTyped/DefinitelyTyped/pull/56687
 import Latex from 'react-latex'
 
 interface RenderedTextProps {
