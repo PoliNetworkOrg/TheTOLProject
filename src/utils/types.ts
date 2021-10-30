6a7ac39c5361362a625d9b93c35c015864463649
Federico Grandi <fgrandi30@gmail.com>
2021-10-30 10:00:21 +0200

    chore: remove unuesd export

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,4 +1,4 @@
 import React from 'react'
 
-export type stateSetter<T> = React.Dispatch<React.SetStateAction<T>>
+type stateSetter<T> = React.Dispatch<React.SetStateAction<T>>
 export type statePair<T> = [T, stateSetter<T>]
