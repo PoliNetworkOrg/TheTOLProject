95d4c4747942a43edeee51c10b4b9cc106c41936
Federico Grandi <fgrandi30@gmail.com>
2021-11-01 13:56:37 +0100

    fix: style InfoStart

--- src/components/InfoView/InfoStart.tsx
+++ src/components/InfoView/InfoStart.tsx
@@ -1,17 +1,27 @@
 import React from 'react'
+import { createStyle } from '../../utils/style'
 import Button from '../Util/Button'
+
+const divStyle = createStyle({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  margin: '10px'
+})
 
 interface InfoStartProps {
   startTest: () => void
 }
 export default function InfoStart(props: InfoStartProps) {
   return (
-    <div>
+    <div style={divStyle}>
       <p>
         Questa è una serie di informazioni veramente interessanti sulla demo,
         che prima o poi mi metterò a scrivere
       </p>
-      <Button label="Inizia il test" onClick={props.startTest} />
+      <div>
+        <Button label="Inizia il test" onClick={props.startTest} />
+      </div>
     </div>
   )
 }
