166c7f6de446f78b9b3fb5f9cfc7fe9bb0029fe9
Federico Grandi <fgrandi30@gmail.com>
2021-11-01 14:42:00 +0100

    feat: improve website header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,17 +3,42 @@ import { createStyle } from '../utils/style'
 import { statePair } from '../utils/types'
 import { view } from './App'
 import Button from './Util/Button'
+import logo from '../static/logo.webp'
 
 const divStyle = createStyle({
   height: '70px',
   display: 'flex',
-  alignItems: 'baseline',
+  alignItems: 'center',
   justifyContent: 'space-between'
 })
 
 const textStyle = createStyle({
+  flex: 1,
   fontSize: '18pt',
   display: 'inline-block'
+})
+
+const centeredText = createStyle(textStyle, {
+  display: 'flex',
+  justifyContent: 'center'
+})
+
+const logoDivStyle = createStyle({
+  flex: 1,
+  display: 'flex',
+  alignItems: 'center',
+  gap: '10px',
+  margin: '5px'
+})
+
+const logoStyle = createStyle({
+  height: '60px'
+})
+
+const buttonDivStyle = createStyle({
+  display: 'flex',
+  flex: 1,
+  justifyContent: 'flex-end'
 })
 
 interface HeaderProps {
@@ -23,17 +48,21 @@ interface HeaderProps {
 export default function Header({ viewState }: HeaderProps) {
   return (
     <div style={divStyle}>
-      <h1 style={textStyle}>
-        PoliNetwork banner placeholder - The TOL Project
-      </h1>
-      <Button
-        label="Toggle demo view"
-        onClick={() => {
-          const [currentView, selectView] = viewState
-          if (currentView == 'dbPreview') selectView('INFO-start')
-          else selectView('dbPreview')
-        }}
-      />
+      <div style={logoDivStyle}>
+        <img src={logo} alt="logo" style={logoStyle} />
+        <h1 style={textStyle}>PoliNetwork</h1>
+      </div>
+      <h1 style={centeredText}>The TOL Project</h1>
+      <div style={buttonDivStyle}>
+        <Button
+          label="Toggle demo view"
+          onClick={() => {
+            const [currentView, selectView] = viewState
+            if (currentView == 'dbPreview') selectView('INFO-start')
+            else selectView('dbPreview')
+          }}
+        />
+      </div>
     </div>
   )
 }
