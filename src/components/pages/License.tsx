34a2e8810ff7199f491004b5ff611c04331141a9
Federico Grandi <fgrandi30@gmail.com>
2021-12-06 20:33:38 +0100

    fix: update License page

--- src/components/pages/License.tsx
+++ src/components/pages/License.tsx
@@ -5,6 +5,9 @@ const styles = StyleSheet.create({
   div: {
     paddingBlock: '15px'
   },
+  centered: {
+    textAlign: 'center'
+  },
   licenseImg: {
     borderWidth: 0,
     marginLeft: 'auto',
@@ -27,42 +30,84 @@ export function License() {
           src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png"
         />
       </a>
-      <br />
-      <span
-        {...{ 'xmlns:dct': 'http://purl.org/dc/terms/' }}
-        property="dct:title"
-      >
-        TheTOLProject
-      </span>{' '}
-      by{' '}
-      <a
-        {...{ 'xmlns:cc': 'http://creativecommons.org/ns#' }}
-        href="https://polinetwork.github.io/it/projects/"
-        property="cc:attributionName"
-        rel="cc:attributionURL noreferrer noopener"
-        target="_blank"
-      >
-        PoliNetwork
-      </a>{' '}
-      is licensed under a{' '}
-      <a
-        rel="license noreferrer noopener"
-        href="http://creativecommons.org/licenses/by-sa/4.0/"
-        target="_blank"
-      >
-        Creative Commons Attribution-ShareAlike 4.0 International License
-      </a>
-      .<br />
-      <br />
-      Permissions beyond the scope of this license may be available at{' '}
-      <a
-        {...{ 'xmlns:cc': 'http://creativecommons.org/ns#' }}
-        href="https://github.com/PoliNetworkOrg/TheTOLProject/blob/main/LICENSE"
-        rel="cc:morePermissions noreferrer noopener"
-        target="_blank"
-      >
-        https://github.com/PoliNetworkOrg/TheTOLProject/blob/main/LICENSE
-      </a>
+      <p>
+        <span
+          {...{ 'xmlns:dct': 'http://purl.org/dc/terms/' }}
+          property="dct:title"
+        >
+          TheTOLProject
+        </span>{' '}
+        by{' '}
+        <a
+          {...{ 'xmlns:cc': 'http://creativecommons.org/ns#' }}
+          href="https://polinetwork.github.io/it/projects/"
+          property="cc:attributionName"
+          rel="cc:attributionURL noreferrer noopener"
+          target="_blank"
+        >
+          PoliNetwork
+        </a>{' '}
+        è rilasciato sotto una linceza{' '}
+        <a
+          rel="license noreferrer noopener"
+          href="http://creativecommons.org/licenses/by-sa/4.0/"
+          target="_blank"
+        >
+          Creative Commons Attribution-ShareAlike 4.0 International License
+        </a>
+        .
+        <br />
+        Permessi che vanno oltre l'ambito di questa licenza posso essere
+        disponibili qui:{' '}
+        <a
+          {...{ 'xmlns:cc': 'http://creativecommons.org/ns#' }}
+          href="https://github.com/PoliNetworkOrg/TheTOLProject/blob/main/LICENSE"
+          rel="cc:morePermissions noreferrer noopener"
+          target="_blank"
+        >
+          https://github.com/PoliNetworkOrg/TheTOLProject/blob/main/LICENSE
+        </a>
+        .
+      </p>
+      <p style={styles.centered}>---</p>
+      <p>
+        <span
+          {...{ 'xmlns:dct': 'http://purl.org/dc/terms/' }}
+          property="dct:title"
+        >
+          TheTOLProject
+        </span>{' '}
+        by{' '}
+        <a
+          {...{ 'xmlns:cc': 'http://creativecommons.org/ns#' }}
+          href="https://polinetwork.github.io/it/projects/"
+          property="cc:attributionName"
+          rel="cc:attributionURL noreferrer noopener"
+          target="_blank"
+        >
+          PoliNetwork
+        </a>{' '}
+        is licensed under a{' '}
+        <a
+          rel="license noreferrer noopener"
+          href="http://creativecommons.org/licenses/by-sa/4.0/"
+          target="_blank"
+        >
+          Creative Commons Attribution-ShareAlike 4.0 International License
+        </a>
+        .
+        <br />
+        Permissions beyond the scope of this license may be available at{' '}
+        <a
+          {...{ 'xmlns:cc': 'http://creativecommons.org/ns#' }}
+          href="https://github.com/PoliNetworkOrg/TheTOLProject/blob/main/LICENSE"
+          rel="cc:morePermissions noreferrer noopener"
+          target="_blank"
+        >
+          https://github.com/PoliNetworkOrg/TheTOLProject/blob/main/LICENSE
+        </a>
+        .
+      </p>
     </div>
   )
 }
