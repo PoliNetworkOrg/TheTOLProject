3af0f4f55175dd74b908396c222c27ab74ed3357
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2023-03-27 13:18:29 +0200

    Implement the privacy rule regarding local storage
    Rule: preferences older than 6 months should be deleted.
    Now local storage values are cleared according to this rule
    (actually only `is_dsa` is handled, because it is the only value stored
    in local storage).

--- src/utils/contexts.tsx
+++ src/utils/contexts.tsx
@@ -15,23 +15,39 @@ export const TestContext = createContext<ITestContext>({
   }
 })
 
+function isDateOlderThanSixMonths(date: Date): boolean {
+  const sixMonthsAgo = new Date()
+  sixMonthsAgo.setMonth(sixMonthsAgo.getMonth() - 6)
+
+  return date < sixMonthsAgo
+}
+
 type TestProviderProps = React.HTMLAttributes<React.ProviderProps<ITestContext>>
 export const TestProvider = (props: TestProviderProps) => {
   const [isDsa, setIsDsa] = useState<boolean>(false)
 
   const toggleDsa = () => {
     setIsDsa((prev) => {
-      const val = !prev
-      localStorage.setItem('is_dsa', JSON.stringify(val))
-      return val
+      const value = !prev
+      const date = new Date()
+      const data = { value, date }
+      localStorage.setItem('is_dsa', JSON.stringify(data))
+      return value
     })
   }
 
   useEffect(() => {
     const localValue = localStorage.getItem('is_dsa')
     if (localValue === null) return
-    const boolValue = JSON.parse(localValue)
-    setIsDsa(boolValue)
+    const { date, value } = JSON.parse(localValue)
+    const changeDate = new Date(date)
+    if (isDateOlderThanSixMonths(changeDate)) {
+      // the preference is older than 6 months ago, so it should be ignored and deleted
+      localStorage.removeItem('is_dsa')
+    } else {
+      // set the current value to saved one
+      setIsDsa(value)
+    }
   }, [])
 
   return <TestContext.Provider value={{ isDsa: isDsa, toggleDsa }} {...props} />
