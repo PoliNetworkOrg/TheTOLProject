d8f64a58e5333763ccdab382236cf34e2b7dee89
Tommaso Morganti <tommaso.morganti@mail.polimi.it>
2024-03-08 17:08:35 +0100

    fix: update admission rules for 2024/2025 (#118)
    * fix: update adminssion rules for 2024/2025
    
    * fix: fix linting errors
    
    * fix: bold words
    
    * feat: add link to rankings in results
    
    * refactor: parametrized threshold values in i18n string

--- src/components/pages/Results.tsx
+++ src/components/pages/Results.tsx
@@ -5,9 +5,10 @@ import {
   getSectionName,
   sectionInfo,
   tengPassThreshold,
-  testPassThreshold,
+  standardTestPassThreshold,
   testTotalScore,
-  View
+  View,
+  earlyTestPassThreshold
 } from '../../utils/constants'
 import { Question, QuestionsData, Section } from '../../utils/database'
 import { formatNumber, StyleSheet, theme } from '../../utils/style'
@@ -78,6 +79,12 @@ const styles = StyleSheet.create({
   restartButton: {
     margin: '12px',
     marginBottom: '32px'
+  },
+  link: {
+    display: 'inline-flex',
+    color: theme.primary,
+    gap: '5px',
+    alignItems: 'center'
   }
 })
 
@@ -169,7 +176,8 @@ export default function Results(props: ResultsProps) {
     .reduce((acc, curr) => acc.add(curr), new Fraction(0))
 
   const roundedScore = parseInt(score.round(0).toString(0))
-  const testPassed = roundedScore >= testPassThreshold,
+  const standardTestPassed = roundedScore >= standardTestPassThreshold,
+    earlyTestPassed = roundedScore >= earlyTestPassThreshold,
     tengPassed = correctionGrid.ing?.correct >= tengPassThreshold
 
   const resultTable = () => (
@@ -177,11 +185,12 @@ export default function Results(props: ResultsProps) {
       <p style={styles.centeredText}>
         <br />
         TOL{' '}
-        {testPassed
-          ? `${t('results.test.passed')} ${!tengPassed ? ' (OFA TENG)' : ''}`
-          : `${t('results.test.failed')}: OFA TEST${
-              !tengPassed ? ' + OFA TENG' : ''
-            }`}
+        {earlyTestPassed
+          ? t('results.test.earlyPassed')
+          : standardTestPassed
+            ? t('results.test.standardPassed')
+            : t('results.test.failed')}
+        {!tengPassed && ' (OFA TENG)'}
         <br />
         {t('results.test.points')}: {formatNumber(score)} /{' '}
         {formatNumber(testTotalScore)} ({formatNumber(score, true)})
@@ -250,9 +259,20 @@ export default function Results(props: ResultsProps) {
               i18n={i18n}
               values={{
                 v1: formatNumber(testTotalScore, true),
-                v2: formatNumber(testPassThreshold),
-                v3: sectionInfo.ing.name,
-                v4: formatNumber(tengPassThreshold)
+                v2: formatNumber(standardTestPassThreshold),
+                v3: formatNumber(earlyTestPassThreshold),
+                v4: sectionInfo.ing.name,
+                v5: formatNumber(tengPassThreshold)
+              }}
+              components={{
+                rankingsLink: (
+                  <a
+                    href="https://rankings.polinetwork.org/#/home/Ingegneria"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                    style={styles.link}
+                  />
+                )
               }}
             >
               results.pointsCalc.body.1
